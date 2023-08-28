72e8f99b42eb3f933e3545aae7841cc11b091e77
miracle-halation <miraclehalation.9710@gmail.com>
2023-08-28 19:02:58 +0900

    Pokemonのmigrationとモデルを作成

---
+++ backend/db/migrate/20230828094830_create_pokemons.rb
@@ -0,0 +1,19 @@
+class CreatePokemons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pokemons do |t|
+      t.string    :name, null: false, limit: 255
+      t.integer   :hp, null: false
+      t.integer   :attack, null: false
+      t.integer   :block, null: false
+      t.integer   :contact, null: false
+      t.integer   :diffence, null: false
+      t.integer   :speed, null: false
+      t.string    :type1, null: false, limit: 255
+      t.string    :type2, limit: 255
+      t.string    :avility1, null: false, limit: 255
+      t.string    :avility2, limit: 255
+      t.string    :hidden_avility, limit: 255
+      t.timestamps
+    end
+  end
+end
