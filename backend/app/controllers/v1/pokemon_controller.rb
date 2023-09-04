c6ceb43575536ce6279438c73e128a1d152046b3
miracle-halation <miraclehalation.9710@gmail.com>
2023-09-04 22:47:06 +0900

    コントローラーの登録などの処理を全てserviceに分離

--- backend/app/controllers/v1/pokemon_controller.rb
+++ backend/app/controllers/v1/pokemon_controller.rb
@@ -1,44 +1,44 @@
 class V1::PokemonController < ApplicationController
-	def index
-		@pokemons = Pokemon.all
-		render json: { pokemons: @pokemons }, status: :ok
-	end
+  def index
+    @pokemons = PokemonService.all_pokemons
+    render json: { pokemons: @pokemons }, status: :ok
+  end
 
-	def show
-		@pokemon = Pokemon.find(params[:id])
-		render json: { pokemon: @pokemon }, status: :ok
-	end
+  def show
+    @pokemon = PokemonService.find_pokemon(params[:id])
+    render json: { pokemon: @pokemon }, status: :ok
+  end
 
-	def create
-		@pokemon = Pokemon.new(pokemon_params)
-		if @pokemon.save
-			render json: { message: 'Successfully created', pokemon: @pokemon }, status: :ok
-		else
-			render json: { message: 'Failed to create Pokemon.' }, status: :unprocessable_entity
-		end
-	end
+  def create
+    @pokemon = PokemonService.create_pokemon(pokemon_params)
+    if @pokemon.persisted?
+      render json: { message: 'Successfully created', pokemon: @pokemon }, status: :ok
+    else
+      render json: { message: 'Failed to create Pokemon.' }, status: :unprocessable_entity
+    end
+  end
 
-	def update
-		@pokemon = Pokemon.find(params[:id])
-		if @pokemon.update(pokemon_params)
-			render json: { message: 'Successfully updated', pokemon: @pokemon }, status: :ok
-		else
-			render json: { message: 'Failed to update Pokemon.' }, status: :unprocessable_entity
-		end
-	end
+  def update
+    @pokemon = PokemonService.find_pokemon(params[:id])
+    if PokemonService.update_pokemon(@pokemon, pokemon_params)
+      render json: { message: 'Successfully updated', pokemon: @pokemon }, status: :ok
+    else
+      render json: { message: 'Failed to update Pokemon.' }, status: :unprocessable_entity
+    end
+  end
 
-	def destroy
-		@pokemon = Pokemon.find(params[:id])
-		if @pokemon.destroy
+  def destroy
+    @pokemon = PokemonService.find_pokemon(params[:id])
+    if PokemonService.delete_pokemon(@pokemon)
       render json: { message: 'Pokemon was successfully deleted.' }, status: :ok
     else
       render json: { message: 'Failed to delete Pokemon.' }, status: :unprocessable_entity
     end
-	end
+  end
 
-	private
+  private
 
-	def pokemon_params
-		params.require(:pokemon).permit(:name, :hp, :attack, :block, :contact, :diffence, :speed, :type1, :type2, :avility1, :avility2, :hidden_avility)
-	end
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :hp, :attack, :block, :contact, :diffence, :speed, :type1, :type2, :avility1, :avility2, :hidden_avility)
+  end
 end
