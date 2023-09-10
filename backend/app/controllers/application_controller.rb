d464fb4e462d7df01fd56d21614d659ebb0ebc1e
miracle-halation <miraclehalation.9710@gmail.com>
2023-09-10 10:55:36 +0900

    rails側での認証を実装することに成功

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
+require 'jwt'
+
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    token = request.headers['Authorization']&.split(' ')&.last # Authorizationヘッダーからトークンを取得
+
+    if token
+      begin
+        decoded_token = JWT.decode(token, nil, false)
+        uid = decoded_token[0]['sub'] # Firebase UIDを取得
+        # Firebaseに対して認証を行うか、Firebase UIDを使用してユーザーを特定します。
+        user = User.find_by(uid: uid)
+
+        if user
+          @current_user = user
+        else
+          self.not_authenticated
+        end
+      rescue JWT::DecodeError
+        self.not_authenticated
+      end
+    else
+      self.not_authenticated
+    end
+  end
+
+  private
+
+  def not_authenticated
+    render json: { error: { messages: ["ログインしてください"] } }, status: :unauthorized
+  end
 end
