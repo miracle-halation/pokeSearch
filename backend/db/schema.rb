3cdf955e9cf17bdcb5c61809e9b2a239363aa1cd
miracle-halation <miraclehalation.9710@gmail.com>
2023-09-30 18:21:49 +0900

    typeモデルを作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_134124) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_30_084307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_134124) do
     t.string "avility1", limit: 255, null: false
     t.string "avility2", limit: 255
     t.string "hidden_avility", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
